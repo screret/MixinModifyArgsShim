1fd5c960411eb55098c0893a92337cb3177a571e
screret <68943070+screret@users.noreply.github.com>
2026-06-12 13:35:33 +0300

    Add neoforge compatibility IDK if this is even needed..

--- src/main/java/org/spongepowered/asm/synthetic/args/Dummy.java
+++ src/main/java/org/spongepowered/asm/synthetic/args/Dummy.java
@@ -1,9 +1,16 @@
 package org.spongepowered.asm.synthetic.args;
 
-import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.IExtensionPoint;
+import net.minecraftforge.fml.ModLoadingContext;
 
-@Mod("mixin_args")
+@net.minecraftforge.fml.common.Mod("mixin_args")
 public class Dummy {
 
-    public Dummy() {}
+    // copied from FMLNetworkConstants because I can't figure out what maven artifact the class is from
+    private static final String IGNORESERVERONLY = "OHNOES\uD83D\uDE31\uD83D\uDE31\uD83D\uDE31\uD83D\uDE31\uD83D\uDE31\uD83D\uDE31\uD83D\uDE31\uD83D\uDE31\uD83D\uDE31\uD83D\uDE31\uD83D\uDE31\uD83D\uDE31\uD83D\uDE31\uD83D\uDE31\uD83D\uDE31\uD83D\uDE31\uD83D\uDE31";
+
+    public Dummy() {
+        ModLoadingContext.get().registerExtensionPoint(IExtensionPoint.DisplayTest.class,
+                () -> new IExtensionPoint.DisplayTest(() -> IGNORESERVERONLY, (incoming, isNetwork) -> true));
+    }
 }
---
+++ src/main/java/org/spongepowered/asm/synthetic/args/DummyNeoForge.java
@@ -0,0 +1,7 @@
+package org.spongepowered.asm.synthetic.args;
+
+@net.neoforged.fml.common.Mod("mixin_args")
+public class DummyNeoForge {
+
+    public DummyNeoForge() {}
+}
